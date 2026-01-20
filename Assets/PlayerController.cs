472d558129cfa9920318614fffc5f65cf83c6036
Lenophie <lenophie.dev@gmail.com>
2026-01-20 12:12:18 +0100

    Add inventory logic

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform playerTransform;
     [SerializeField] private float speed = 1f;
 
-    [SerializeField] private Seed seed;
+    [SerializeField] private List<Seed> startSeeds;
+
+    private List<Seed> seeds;
+    private int selectionIndex;
+
+    private void Start()
+    {
+        seeds = new List<Seed>(startSeeds);
+        selectionIndex = 0;
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,11 +26,21 @@ public class PlayerController : MonoBehaviour
 
         playerTransform.position += (Vector3) movement * Time.deltaTime * speed;
 
+        if (playerInput.actions["SwitchItemLeft"].WasPerformedThisFrame()) {
+            selectionIndex--;
+            if (selectionIndex < 0)
+                selectionIndex = seeds.Count - 1;
+        } else if (playerInput.actions["SwitchItemRight"].WasPerformedThisFrame()) {
+            selectionIndex++;
+            if (selectionIndex >= seeds.Count)
+                selectionIndex = 0;
+        }
+
         RaycastHit2D hit = Physics2D.Raycast(
-            playerTransform.position - Vector3.forward,
-            Vector3.forward,
-            2f,
-            LayerMask.GetMask("Plantation"));
+                playerTransform.position - Vector3.forward,
+                Vector3.forward,
+                2f,
+                LayerMask.GetMask("Plantation"));
         if (hit)
         {
             PlantationController plantation = hit.collider.GetComponent<PlantationController>();
@@ -28,7 +48,15 @@ public class PlayerController : MonoBehaviour
             if (playerInput.actions["Interact"].WasPerformedThisFrame())
             {
                 if (plantation.plantedSeed == null)
-                    plantation.Plant(seed);
+                {
+                    if (seeds.Count > 0)
+                    {
+                        plantation.Plant(seeds[selectionIndex]);
+                        seeds.RemoveAt(selectionIndex);
+                        if (selectionIndex != 0)
+                            selectionIndex--;
+                    }
+                }
                 else if (plantation.IsReadyToHarvert)
                 {
                     // On peut faire des trucs avec la récolte!!
